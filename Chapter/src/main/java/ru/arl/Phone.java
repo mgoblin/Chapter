999005d41afd614bf3906065dedf6d1a6f8d231f
AkopyaSHa <akopyasha@bk.ru>
2020-04-06 21:37:12 +0300

    правки

--- Chapter/src/main/java/ru/arl/Phone.java
+++ Chapter/src/main/java/ru/arl/Phone.java
@@ -19,8 +19,8 @@ public class Phone {
         new Phone ("нет","нет",0);
     }
 
-    public void receiveCall(name, number) {
-        String name = "Вам звонит " + name + number;
+    public void receiveCall(String name) {
+        String name = "Вам звонит " + name;
     }
 
     //Геттеры и Сеттеры
