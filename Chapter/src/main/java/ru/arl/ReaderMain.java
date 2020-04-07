0769b644a0aa4873cfbb21087a65b40e8acfcddc
AkopyaSHa <akopyasha@bk.ru>
2020-04-07 22:11:14 +0300

    Правки

--- Chapter/src/main/java/ru/arl/ReaderMain.java
+++ Chapter/src/main/java/ru/arl/ReaderMain.java
@@ -4,9 +4,9 @@ public class ReaderMain {
     public static void main(Reader[] args) {
 
         Reader[] readers = new Reader[3];
-        readers[0] = "Энциклопедия";
-        readers[1] = "Приключения";
-        readers[2] = "Словарь";
+        readers[0] = new Reader;
+        readers[1] = new Reader;
+        readers[2] = new Reader;
 
         int sumBook = readers.length;
         System.out.println(sumBook);
