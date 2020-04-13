2c0bb4b030cd1e6f9ae00c9b04ea6418419eaf00
mgoblin <mike.golovanov@gmail.com>
2020-04-13 21:15:35 +0300

    numbers implementation3

--- Chapter/src/main/java/ru/arl/numbers/NumbersMain.java
+++ Chapter/src/main/java/ru/arl/numbers/NumbersMain.java
@@ -6,7 +6,8 @@ public class NumbersMain {
         System.out.println("Печатаем числа");
         if (a <= b) {
             for(int i = a; i <= b; i++) {
-                System.out.println(i > 0 ? i : -i);
+                if (i > 0) System.out.println(i);
+                else System.out.println(-i);
             }
         } else {
             printNumbers(-a, -b);
