a5f87e9a2a371aff5e1982ae8ed03c76ed2ec3b1
mgoblin <mike.golovanov@gmail.com>
2020-04-13 20:35:56 +0300

    numbers draft

---
+++ Chapter/src/main/java/ru/arl/numbers/NumbersMain.java
@@ -0,0 +1,15 @@
+package ru.arl.numbers;
+
+public class NumbersMain {
+
+    public void printNumbers(int a, int b) {
+
+    }
+
+    public static void main(String[] args) {
+        final NumbersMain numbersMain = new NumbersMain();
+
+        numbersMain.printNumbers(10, 20);
+        numbersMain.printNumbers(15, 7);
+    }
+}
