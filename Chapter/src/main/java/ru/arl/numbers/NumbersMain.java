292acf4644ed49ca13a9920d14cb12e65dfc7d47
mgoblin <mike.golovanov@gmail.com>
2020-04-13 20:45:10 +0300

    numbers implementation

--- Chapter/src/main/java/ru/arl/numbers/NumbersMain.java
+++ Chapter/src/main/java/ru/arl/numbers/NumbersMain.java
@@ -3,7 +3,14 @@ package ru.arl.numbers;
 public class NumbersMain {
 
     public void printNumbers(int a, int b) {
-
+        System.out.println("тут мы будем делать полезное дело");
+        if (a <= b) {
+            for(int i = a; i <= b; i++) {
+                System.out.println(i);
+            }
+        } else {
+            printNumbers(b, a);
+        }
     }
 
     public static void main(String[] args) {
