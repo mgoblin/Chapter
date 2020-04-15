9dead74e9a4e2cfbcea3c3aacdaca008945bc751
mgoblin <mike.golovanov@gmail.com>
2020-04-15 21:36:55 +0300

    cars

--- Chapter/src/main/java/ru/arl/task8/Vet.java
+++ Chapter/src/main/java/ru/arl/task8/Vet.java
@@ -1,20 +1,20 @@
 package ru.arl.task8;
 
 public class Vet{
-    public void treatAnimal(Animal animal) {
-        System.out.println(food +' '+ location);
-    }
-
-
-    public static void main(String[] args) {
-        Animal[] animal = new Animal[3];
-        animal[0]=new Cat("Кош.корм","Дома",4,1);
-        animal[1]=new Dog("Собач.корм","На выставке",4,1);
-        animal[2]=new Horse("Сено","Хлев","Серая, длинная",4,1);
-
-        for (int i=0; i<animal.length; i++){
-            animal[i].treatAnimal;
-        }
-
-    }
+//    public void treatAnimal(Animal animal) {
+//        System.out.println(food +' '+ location);
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        Animal[] animal = new Animal[3];
+//        animal[0]=new Cat("Кош.корм","Дома",4,1);
+//        animal[1]=new Dog("Собач.корм","На выставке",4,1);
+//        animal[2]=new Horse("Сено","Хлев","Серая, длинная",4,1);
+//
+//        for (int i=0; i<animal.length; i++){
+//            animal[i].treatAnimal;
+//        }
+//
+//    }
 }
