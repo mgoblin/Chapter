819d3f3652b8322c975b22010806bdfacefe209f
mgoblin <mike.golovanov@gmail.com>
2020-04-16 21:55:49 +0300

    shapes

--- Chapter/src/main/java/ru/arl/task11/Shape.java
+++ Chapter/src/main/java/ru/arl/task11/Shape.java
@@ -12,22 +12,24 @@ public abstract class Shape {
     public String getColour() {
         return colour;
     }
+
     public void setColour(String colour) {
         this.colour = colour;
     }
 
     public static void drawShapes(Shape[] shapes) {
-        for(Shape shape: shapes) {
+        for (Shape shape : shapes) {
             shape.draw();
         }
     }
 
     public static void main(String[] args) {
-        Shape[] figures= new Shape[4];
-        figures[0]=new Circle("Белый",10,10);
-        figures[1]=new Rectangle("Черный",1,2);
-        figures[2] = new Triangle("Синий", 1);
-        figures[3]=new Star("Яркая");
+        Shape[] figures = new Shape[] {
+                new Circle("Белый", 10, 10),
+                new Rectangle("Черный", 1, 2),
+                new Triangle("Синий", 1),
+                new Star("Яркая")
+        };
 
         drawShapes(figures);
     }
