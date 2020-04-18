e94f03469976429369024d7c1025d4c77fe44a1c
AkopyaSha <akopyasha@bk.ru>
2020-04-18 21:33:12 +0300

    Задание Робот

--- Chapter/src/main/java/ru/arl/robot/Robot.java
+++ Chapter/src/main/java/ru/arl/robot/Robot.java
@@ -4,6 +4,22 @@ public class Robot {
     private int x=0;
     private int y=0;
 
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public Robot(int x, int y) {
         this.x = x;
         this.y = y;
