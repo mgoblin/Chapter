078bf4a754e6db1ef8dd0adde0a4be04ad008883
mgoblin <mike.golovanov@gmail.com>
2020-04-21 22:23:56 +0300

    board

--- Chapter/src/main/java/ru/arl/exceptions/TestExceptions.java
+++ Chapter/src/main/java/ru/arl/exceptions/TestExceptions.java
@@ -4,7 +4,12 @@ public class TestExceptions {
 
     public int divide(int i, int j) {
         if (j == 0) {
-            throw new Exception("Нельзя делить на 0!!!");
+            try {
+                throw new Exception("Нельзя делить на 0!!!");
+            } catch (Exception e) {
+                System.out.println("Я знаю ты делил на ноль. Так нельзя");
+                return 0;
+            }
         } else {
             return i / j;
         }
