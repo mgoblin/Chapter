5043dcb71f0a3911618baaaed6ceb52273d6c7cc
mgoblin <mike.golovanov@gmail.com>
2020-04-21 22:40:21 +0300

    board

--- Chapter/src/main/java/ru/arl/exceptions/TestExceptions.java
+++ Chapter/src/main/java/ru/arl/exceptions/TestExceptions.java
@@ -2,15 +2,15 @@ package ru.arl.exceptions;
 
 public class TestExceptions {
 
-    public int divide(int i, int j) {
+    public int divide(int i, int j) throws Exception {
         if (j == 0) {
-            throw new RuntimeException("Нельзя делить на 0!!!");
+            throw new Exception("Нельзя делить на 0!!!");
         } else {
             return i / j;
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         TestExceptions testExceptions = new TestExceptions();
 
         System.out.println("Результат деления: " + testExceptions.divide(0, 0));
