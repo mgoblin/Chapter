427decde48d55294435964b3a44d3a60bab14ed3
mgoblin <mike.golovanov@gmail.com>
2020-04-21 22:15:22 +0300

    board

--- Chapter/src/main/java/ru/arl/exceptions/TestExceptions.java
+++ Chapter/src/main/java/ru/arl/exceptions/TestExceptions.java
@@ -2,7 +2,12 @@ package ru.arl.exceptions;
 
 public class TestExceptions {
 
+    public int divide(int i, int j) {
+        return i/j;
+    }
+
     public static void main(String[] args) {
         TestExceptions testExceptions = new TestExceptions();
+        testExceptions.divide(0, 0);
     }
 }
