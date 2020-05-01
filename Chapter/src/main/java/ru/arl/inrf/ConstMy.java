14c03061a1a9b2ea692fb3de2c780a78daf1a8b3
mgoblin <mike.golovanov@gmail.com>
2020-05-01 21:47:14 +0300

    board

--- Chapter/src/main/java/ru/arl/inrf/ConstMy.java
+++ Chapter/src/main/java/ru/arl/inrf/ConstMy.java
@@ -1,8 +1,13 @@
 package ru.arl.inrf;
 
-public class ConstMy extends Base implements IMyIntreface{
+public class ConstMy extends Base implements IMyIntreface, IIntrf2 {
     @Override
     public String greeting(String name) {
         return "Привет, " + name;
+    }
+
+    @Override
+    public int age() {
+        return 23;
     }
 }
--- Chapter/src/main/java/ru/arl/inrf/Main.java
+++ Chapter/src/main/java/ru/arl/inrf/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         ConstMy constMy = new ConstMy();
         System.out.println(constMy.greeting("Рубен"));
+        System.out.println("Твой возраст: " + constMy.age());
     }
 }
