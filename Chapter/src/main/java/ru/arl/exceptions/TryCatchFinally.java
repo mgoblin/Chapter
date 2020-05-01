3f3c7d05e73a11e75b01195bd56a8430671eaece
mgoblin <mike.golovanov@gmail.com>
2020-05-01 21:16:11 +0300

    board

---
+++ Chapter/src/main/java/ru/arl/exceptions/TryCatchFinally.java
@@ -0,0 +1,13 @@
+package ru.arl.exceptions;
+
+public class TryCatchFinally {
+
+    public void run() {
+        System.out.println("Starting");
+    }
+
+    public static void main(String[] args) {
+        TryCatchFinally program = new TryCatchFinally();
+        program.run();
+    }
+}
