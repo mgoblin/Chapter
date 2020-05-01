507354b85587129ed6e1f551434c2c1eaa483e91
mgoblin <mike.golovanov@gmail.com>
2020-05-01 21:53:38 +0300

    board

---
+++ Chapter/src/main/java/ru/arl/inrf/ConsoleMy.java
@@ -0,0 +1,13 @@
+package ru.arl.inrf;
+
+public class ConsoleMy implements IMyIntreface, IIntrf2 {
+    @Override
+    public String greeting(String name) {
+        return null;
+    }
+
+    @Override
+    public int age() {
+        return 0;
+    }
+}
