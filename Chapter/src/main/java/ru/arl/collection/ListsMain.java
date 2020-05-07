52ec1bac0cdb586ac3bae8b5e1264207badda5cf
mgoblin <mike.golovanov@gmail.com>
2020-05-07 21:42:22 +0300

    lists

--- Chapter/src/main/java/ru/arl/collection/ListsMain.java
+++ Chapter/src/main/java/ru/arl/collection/ListsMain.java
@@ -6,5 +6,9 @@ public class ListsMain {
     public static void main(String[] args) {
         final List<String> names = List.of("Злата", "Тея");
         System.out.println(names);
+
+        System.out.println(names.get(1));
+        names.set(0, "Рубен");
+        System.out.println(names.get(0));
     }
 }
