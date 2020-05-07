0be23cca80067f01c93e0dbab1f45fecf6b23cb0
mgoblin <mike.golovanov@gmail.com>
2020-05-07 21:48:00 +0300

    lists

--- Chapter/src/main/java/ru/arl/collection/ListsMain.java
+++ Chapter/src/main/java/ru/arl/collection/ListsMain.java
@@ -1,11 +1,13 @@
 package ru.arl.collection;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ListsMain {
     public static void main(String[] args) {
-        final List<String> names = new ArrayList<>(List.of("Злата", "Тея"));
+
+        final List<String> names = Arrays.asList("Злата", "Тея");
         System.out.println(names);
 
         System.out.println(names.get(1));
