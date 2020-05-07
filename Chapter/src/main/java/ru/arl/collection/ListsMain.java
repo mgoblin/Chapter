57638b564a4e4d1547279498870c899c93fdb71f
mgoblin <mike.golovanov@gmail.com>
2020-05-07 21:33:16 +0300

    lists

---
+++ Chapter/src/main/java/ru/arl/collection/ListsMain.java
@@ -0,0 +1,14 @@
+package ru.arl.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListsMain {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Рубен");
+        names.add("Михаил");
+
+        System.out.println(names);
+    }
+}
