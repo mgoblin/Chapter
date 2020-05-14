74dcee6d17fb82f46ac7b45043f17020adbfd837
AkopyaSha <akopyasha@bk.ru>
2020-05-14 21:15:59 +0300

    Практика

--- Chapter/src/main/java/ru/arl/Main.java
+++ Chapter/src/main/java/ru/arl/Main.java
@@ -18,4 +18,75 @@ public class Main {
             }
         System.out.println(sum);
     }
+
+//    package lesson1;
+//
+//    public class Task1_8 {
+//        public static void main(String[] args) {
+//            Task1_8 year = new Task1_8();
+//            year.leapYear(2000);
+//            year.leapYear(1000);
+//            year.leapYear(2016);
+//            year.leapYear(2021);
+//
+//            System.out.println(' ');
+//
+//            year.leapYear1(2000);
+//            year.leapYear1(1000);
+//            year.leapYear1(2016);
+//            year.leapYear1(2021);
+//
+//            System.out.println(' ');
+//
+//            year.leapYear2(2000);
+//            year.leapYear2(1000);
+//            year.leapYear2(2016);
+//            year.leapYear2(2021);
+//
+//            System.out.println(' ');
+//
+//            year.leapYear3(2000);
+//            year.leapYear3(1000);
+//            year.leapYear3(2016);
+//            year.leapYear3(2021);
+//        }
+//
+//        public void leapYear3(int year) {
+//            System.out.print(year);
+//            if (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0)) {
+//                System.out.print(" не");
+//            }
+//            System.out.println(" високосный год");
+//        }
+//
+//        public void leapYear2(int year) {
+//            String not = year % 4 != 0 || (year % 100 == 0 && year % 400 != 0) ? "не " : "";
+//            System.out.println(year + " " + not + "високосный год");
+//        }
+//
+//        public void leapYear1(int year) {
+//            if (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0)) {
+//                System.out.println(year + " не високосный год");
+//            } else {
+//                System.out.println(year + " високосный год");
+//            }
+//        }
+//
+//        public void leapYear(int year) {
+//            if (year % 4 == 0) {
+//                if (year % 100 == 0) {
+//                    if (year % 400 == 0) {
+//                        System.out.println(year + " високосный год");
+//                    } else {
+//                        System.out.println(year + " не високосный год");
+//                    }
+//                } else {
+//                    System.out.println(year + " високосный год");
+//                }
+//            } else {
+//                System.out.println(year + " не високосный год");
+//            }
+//        }
+//    }
+
 }
---
+++ Chapter/src/main/java/task1_1/Task3_1.java
@@ -0,0 +1,34 @@
+package task1_1;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Task3_1 {
+    public static void main(String[] args) {
+        guessNumber();
+    }
+    private static Random random = new Random();
+
+    private static void guessNumber() {
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            int num = random.nextInt(10);
+            System.out.println("Угадай число с 3 попыток от 0 до 9");
+
+            for (int i = 3; i > 0; i--) {
+                System.out.println("Введи свой ответ:");
+                int questionNum = scanner.nextInt();
+                if (questionNum == num) {
+                    System.out.println("Вы угадали число!");
+                    break;
+                }
+                System.out.println(questionNum > num ? "Введенное число больше " : "Введенное число меньше ");
+                System.out.println((i - 1) > 0 ? " у вас осталось " + (i - 1) + " попыток." : "Попытки закончились, вы проиграли.");
+            }
+            System.out.println("Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет)");
+        }
+        while (scanner.nextInt() == 1);
+    }
+}
+
